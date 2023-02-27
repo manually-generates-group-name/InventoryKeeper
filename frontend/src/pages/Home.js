2ec2f3b709c6cd621b35e111f6c41bc75271672a
Angel Zuniga <angel.zuniga1067@gmail.com>
2023-02-27 10:19:13 -0800

    Added linked to group member's APIs

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,9 @@ const Home = () => {
     return(
         <div className="Alex">
             <h1>Hello World!</h1>
+          <p><a href="./pages/AlexAPI.js">Alejandro's API</a></p>
+          <p><a href="./pages/AngelAPI.js">Angel's API</a></p>
+          <p><a href="./pages/AngelAPI.js">Drake's API</a></p>
         </div>
     )
 }
