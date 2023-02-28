bdeaccecfafd8fa2a03fa4f7eaa3d66350b5a2b2
amora7741 <mora7741@gmail.com>
2023-02-27 21:06:48 -0800

    Slight modifications to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,22 @@
+const dotenv = require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const Router = require("./routes")
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-
-const username = "amora7741";
-const password = "fhgHhhfZYzfMWhYA";
-const cluster = "cluster0.nyreuwv";
-const dbname = "test";
-
-const db = mongoose.connection;
 
 app.use(express.json());
 
 mongoose.connect(
-  `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
+  process.env.MONGO_URI, 
   {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   }
 );
+  
+const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
@@ -26,6 +25,6 @@ db.once("open", function () {
 
 app.use(Router);
 
-app.listen(3000, () => {
-  console.log("Server is running at port 3000");
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
 });
