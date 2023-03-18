60b03c1f09d6b59ba6f3b27a06765e34bab72d3d
amora7741 <mora7741@gmail.com>
2023-03-18 16:40:52 -0700

    Added styling to create list page

--- frontend/src/pages/CreateList.js
+++ frontend/src/pages/CreateList.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import { Button, Input, Heading, Box, List, ListItem, Flex, Center } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import axios from 'axios';
 
 const CreateList = () => {
@@ -34,7 +37,10 @@ const CreateList = () => {
     })
     .then(response => {
       console.log(response);
-      alert('List Created Successfully!');
+      toast.success("List submitted successfully!");
+      setTimeout(() => {
+        window.location.reload();
+      }, 3000);
     })
     .catch(error => {
       console.error(error);
@@ -42,51 +48,51 @@ const CreateList = () => {
   };
 
   return (
-    <div>
-      <h1>Create a List</h1>
-      <ul>
-        {items.map((item, index) => {
-          if (item !== null) {
-            return (
-              <li key={index}>
-                <span>{item.name} - {item.store}</span>
-                <button onClick={() => {
-                  setName(item.name);
-                  setStore(item.store);
-                  setEditMode(true);
-                  setEditIndex(index);
-                }}>Edit</button>
-                <button onClick={() => deleteItem(index)}>Delete</button>
-              </li>
-            )
-          }
-        })}
-      </ul>
-      <div>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Item name"
-        />
-        <input
-          type="text"
-          value={store}
-          onChange={(e) => setStore(e.target.value)}
-          placeholder="Store"
-        />
-        <button onClick={addItem}>{editMode ? 'Update Item' : 'Add Item'}</button>
-        {editMode && (
-          <button onClick={() => {
-            setName('');
-            setStore('');
-            setEditMode(false);
-            setEditIndex(-1);
-          }}>Cancel Edit</button>
-        )}
-      </div>
-      <button onClick={submitList}>Submit List</button>
-    </div>
+    <ChakraProvider>
+      <Box maxW="lg" mx="auto" my={10} p={5} borderWidth="1px" borderRadius="lg" boxShadow="md">
+        <Toaster />
+        <Heading as="h1" size="lg" mb={5} textAlign='center'>Create a List</Heading>
+        <List spacing={3}>
+          {items.map((item, index) => {
+            if (item !== null) {
+              return (
+                <ListItem key={index} display="flex" alignItems="center">
+                  <Box flex="1">
+                    <Flex>
+                      <Box fontWeight="bold">{item.name}</Box>
+                      <Box ml={2} color="gray.500">- {item.store}</Box>
+                    </Flex>
+                  </Box>
+                  <Button size="sm" mr={2} onClick={() => {
+                    setName(item.name);
+                    setStore(item.store);
+                    setEditMode(true);
+                    setEditIndex(index);
+                  }}>Edit</Button>
+                  <Button size="sm" colorScheme="red" onClick={() => deleteItem(index)}>Delete</Button>
+                </ListItem>
+              )
+            }
+          })}
+        </List>
+        <Flex mt={5}>
+          <Input mr={2} flex="1" placeholder="Item Name" value={name} onChange={(e) => setName(e.target.value)} />
+          <Input mr={2} flex="1" placeholder="Store" value={store} onChange={(e) => setStore(e.target.value)} />
+          <Button mr={2} colorScheme="green" onClick={addItem}>{editMode ? 'Update Item' : 'Add Item'}</Button>
+          {editMode && (
+            <Button size="sm" onClick={() => {
+              setName('');
+              setStore('');
+              setEditMode(false);
+              setEditIndex(-1);
+            }}>Cancel Edit</Button>
+          )}
+        </Flex>
+        <Center>
+          <Button mt={5} colorScheme="blue" onClick={submitList}>Submit List</Button>
+        </Center>
+      </Box>
+    </ChakraProvider>
   );
 };
 
