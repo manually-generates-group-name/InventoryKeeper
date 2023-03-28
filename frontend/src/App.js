77821f3a2e7a72a98582b9e716c414a05fb2ed9b
amora7741 <mora7741@gmail.com>
2023-03-28 14:22:24 -0700

    Cleaned up files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home';
-import AlexAPI from './pages/AlexAPI';
-import AngelAPI from './pages/AngelAPI';
-import DrakeAPI from './pages/DrakeAPI';
-import Users from './pages/Users';
-import Demo from './pages/Demo';
-import CreateList from './pages/CreateList';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AlexAPI from "./pages/AlexAPI";
+import AngelAPI from "./pages/AngelAPI";
+import DrakeAPI from "./pages/DrakeAPI";
+import Users from "./pages/Users";
+import CreateList from "./pages/CreateList";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <div className='pages'>
+        <div className="pages">
           <Routes>
-            <Route 
-              path = "/"
-              element={<Home />}
-            />
-          <Route 
-              path = "/alex"
-              element={<AlexAPI />}
-            />
-          <Route
-              path = "/angel"
-              element={<AngelAPI />}
-            />
-          <Route
-              path = "/drake"
-              element={<DrakeAPI />}
-            />
-          <Route 
-              path = "/users"
-              element={<Users />}
-            />
-          <Route
-              path = "/demo"
-              element={<Demo />}
-          />
-          <Route
-              path = "/createList"
-              element={<CreateList />}
-          />
+            <Route path="/" element={<Home />} />
+            <Route path="/alex" element={<AlexAPI />} />
+            <Route path="/angel" element={<AngelAPI />} />
+            <Route path="/drake" element={<DrakeAPI />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/createList" element={<CreateList />} />
           </Routes>
         </div>
       </BrowserRouter>
