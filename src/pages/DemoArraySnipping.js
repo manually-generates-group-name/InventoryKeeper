4f0f72c96391fc1a3c1f12808c514788daa6194c
euclidone <euclidone@users.noreply.github.com>
2023-04-14 08:36:53 +0000

    deploy: a67b99845106a47cc81e34f00027531ed7820144

--- src/pages/DemoArraySnipping.js
+++
@@ -1,6 +0,0 @@
-const _ = require('lodash');
-const demoArraySnipping = (leftD, rightD, array) => {
-    //array.push("banana", "apple", "peach", "plum", "orange", "mango", "pineapple");
-    return _.dropRight(_.drop(array, leftD), rightD) // Perform left drop then right drop
-}
-module.exports = demoArraySnipping;
