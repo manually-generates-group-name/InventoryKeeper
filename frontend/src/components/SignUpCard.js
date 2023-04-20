a97005c00f28d4999bf79fc13e584d39e63c7fdd
amora7741 <mora7741@gmail.com>
2023-04-19 21:46:30 -0700

    Added sign up functionality

--- frontend/src/components/SignUpCard.js
+++ frontend/src/components/SignUpCard.js
@@ -13,17 +13,79 @@ import {
   Text,
   useColorModeValue,
   Link,
+  useToast,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { Link as RouterLink } from "react-router-dom";
+import axios from "axios";
 
 export default function SignupCard() {
   const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+
   const bgColor = useColorModeValue(
     "linear(gray.300 90%, gray.100 200%)",
     "linear(gray.800 90%, gray.700 200%)"
   );
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.username || !formData.password) {
+      toast({
+        title: "Error",
+        description: "Username and password are required.",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    setIsLoading(true);
+
+    await axios
+      .post("http://localhost:3001/signUpAPI", formData)
+      .then((response) => {
+        console.log(response);
+        toast({
+          title: "Success",
+          description: "The user was successfully registered!",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+        setTimeout(() => {
+          window.location.reload();
+        }, 3000);
+      })
+      .catch((error) => {
+        toast({
+          title: "Error",
+          description: "The user was not registered. Please try again.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+        console.error(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   return (
     <Flex
@@ -40,32 +102,18 @@ export default function SignupCard() {
         </Stack>
         <Box rounded={"lg"} bg={bgColor} boxShadow={"lg"} p={8}>
           <Stack spacing={4}>
-            <HStack>
-              <Box>
-                <FormControl id="firstName">
-                  <FormLabel>First Name</FormLabel>
-                  <Input type="text" />
-                </FormControl>
-              </Box>
-              <Box>
-                <FormControl id="lastName">
-                  <FormLabel>Last Name</FormLabel>
-                  <Input type="text" />
-                </FormControl>
-              </Box>
-            </HStack>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
             <FormControl id="user" isRequired>
               <FormLabel>Username</FormLabel>
-              <Input type="user" />
+              <Input type="user" id="username" onChange={handleChange} />
             </FormControl>
             <FormControl id="password" isRequired>
               <FormLabel>Password</FormLabel>
               <InputGroup>
-                <Input type={showPassword ? "text" : "password"} />
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  onChange={handleChange}
+                />
                 <InputRightElement h={"full"}>
                   <Button
                     variant={"ghost"}
@@ -78,8 +126,27 @@ export default function SignupCard() {
                 </InputRightElement>
               </InputGroup>
             </FormControl>
+            <HStack>
+              <Box>
+                <FormControl id="firstName">
+                  <FormLabel>First Name</FormLabel>
+                  <Input type="text" id="firstName" onChange={handleChange} />
+                </FormControl>
+              </Box>
+              <Box>
+                <FormControl id="lastName">
+                  <FormLabel>Last Name</FormLabel>
+                  <Input type="text" id="lastName" onChange={handleChange} />
+                </FormControl>
+              </Box>
+            </HStack>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" id="email" onChange={handleChange} />
+            </FormControl>
             <Stack spacing={10} pt={2}>
               <Button
+                isLoading={isLoading}
                 loadingText="Submitting"
                 size="lg"
                 bg={"blue.400"}
@@ -87,6 +154,7 @@ export default function SignupCard() {
                 _hover={{
                   bg: "blue.500",
                 }}
+                onClick={handleSubmit}
               >
                 Sign up
               </Button>
