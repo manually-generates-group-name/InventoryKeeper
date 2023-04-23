ee95a6c13fac7b473c9d6016946b34c50ad54649
amora7741 <mora7741@gmail.com>
2023-04-23 15:56:22 -0700

    Remove unneeded code

--- backend/routes.js
+++ backend/routes.js
@@ -45,27 +45,5 @@ router.post("/signUpAPI", (req, res) => {
     }
   });
 });
-// ...
-// app.post("/add_user", async (request, response) => {
-//     const user = new userModel(request.body);
-
-//     try {
-//       await user.save();
-//       response.send(user);
-//     } catch (error) {
-//       response.status(500).send(error);
-//     }
-// });
-
-// // ...
-// app.get("/users", async (request, response) => {
-//   const users = await userModel.find({});
-
-//   try {
-//     response.send(users);
-//   } catch (error) {
-//     response.status(500).send(error);
-//   }
-// });
 
 module.exports = router;
