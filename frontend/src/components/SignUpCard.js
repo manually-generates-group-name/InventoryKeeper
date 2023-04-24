8a6c71ea7663df4d74ecfc78149f1d334c8260cc
amora7741 <mora7741@gmail.com>
2023-04-24 14:14:48 -0700

    Added circularprogress to sign up page

--- frontend/src/components/SignUpCard.js
+++ frontend/src/components/SignUpCard.js
@@ -14,6 +14,10 @@ import {
   useColorModeValue,
   Link,
   useToast,
+  CircularProgress,
+  Modal,
+  ModalOverlay,
+  ModalContent,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
@@ -31,6 +35,7 @@ export default function SignupCard() {
     password: "",
   });
   const [isLoading, setIsLoading] = useState(false);
+  const [showCircularProgress, setShowCircularProgress] = useState(false);
   const toast = useToast();
 
   const bgColor = useColorModeValue(
@@ -115,8 +120,10 @@ export default function SignupCard() {
       });
 
       setIsLoading(false);
+      setShowCircularProgress(true);
 
       setTimeout(() => {
+        setShowCircularProgress(false);
         window.location.href = "/login";
       }, 3000);
     } catch (error) {
@@ -132,88 +139,113 @@ export default function SignupCard() {
   };
 
   return (
-    <Flex
-      minH={"100vh"}
-      align={"center"}
-      justify={"center"}
-      bgGradient={bgColor}
-    >
-      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-        <Stack align={"center"}>
-          <Heading fontSize={"4xl"} textAlign={"center"}>
-            Sign Up
-          </Heading>
-        </Stack>
-        <Box rounded={"lg"} bg={bgColor} boxShadow={"lg"} p={8}>
-          <Stack spacing={4}>
-            <FormControl id="user" isRequired>
-              <FormLabel>Username</FormLabel>
-              <Input type="user" id="username" onChange={handleChange} />
-            </FormControl>
-            <FormControl id="password" isRequired>
-              <FormLabel>Password</FormLabel>
-              <InputGroup>
-                <Input
-                  type={showPassword ? "text" : "password"}
-                  id="password"
-                  onChange={handleChange}
-                />
-                <InputRightElement h={"full"}>
-                  <Button
-                    variant={"ghost"}
-                    onClick={() =>
-                      setShowPassword((showPassword) => !showPassword)
-                    }
-                  >
-                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-            </FormControl>
-            <HStack>
-              <Box>
-                <FormControl id="firstName">
-                  <FormLabel>First Name</FormLabel>
-                  <Input type="text" id="firstName" onChange={handleChange} />
-                </FormControl>
-              </Box>
-              <Box>
-                <FormControl id="lastName">
-                  <FormLabel>Last Name</FormLabel>
-                  <Input type="text" id="lastName" onChange={handleChange} />
-                </FormControl>
-              </Box>
-            </HStack>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" id="email" onChange={handleChange} />
-            </FormControl>
-            <Stack spacing={10} pt={2}>
-              <Button
-                isLoading={isLoading}
-                loadingText="Submitting"
-                size="lg"
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-                onClick={handleSubmit}
-              >
-                Sign up
-              </Button>
-            </Stack>
-            <Stack pt={6}>
-              <Text align={"center"}>
-                Already a user?{" "}
-                <Link as={RouterLink} to="/login" color={"blue.400"}>
-                  Login
-                </Link>
-              </Text>
-            </Stack>
+    <>
+      <Modal
+        isOpen={showCircularProgress}
+        onClose={() => {}}
+        closeOnOverlayClick={false}
+        isCentered
+      >
+        <ModalOverlay
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <ModalContent
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            borderRadius="md"
+            boxShadow="none"
+            bg="transparent"
+          >
+            <CircularProgress isIndeterminate color="blue.400" size="80%" />
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
+      <Flex
+        minH={"100vh"}
+        align={"center"}
+        justify={"center"}
+        bgGradient={bgColor}
+      >
+        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"} textAlign={"center"}>
+              Sign Up
+            </Heading>
           </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+          <Box rounded={"lg"} bg={bgColor} boxShadow={"lg"} p={8}>
+            <Stack spacing={4}>
+              <FormControl id="user" isRequired>
+                <FormLabel>Username</FormLabel>
+                <Input type="user" id="username" onChange={handleChange} />
+              </FormControl>
+              <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    onChange={handleChange}
+                  />
+                  <InputRightElement h={"full"}>
+                    <Button
+                      variant={"ghost"}
+                      onClick={() =>
+                        setShowPassword((showPassword) => !showPassword)
+                      }
+                    >
+                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <HStack>
+                <Box>
+                  <FormControl id="firstName">
+                    <FormLabel>First Name</FormLabel>
+                    <Input type="text" id="firstName" onChange={handleChange} />
+                  </FormControl>
+                </Box>
+                <Box>
+                  <FormControl id="lastName">
+                    <FormLabel>Last Name</FormLabel>
+                    <Input type="text" id="lastName" onChange={handleChange} />
+                  </FormControl>
+                </Box>
+              </HStack>
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" id="email" onChange={handleChange} />
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button
+                  isLoading={isLoading}
+                  loadingText="Submitting"
+                  size="lg"
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                  onClick={handleSubmit}
+                >
+                  Sign up
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={"center"}>
+                  Already a user?{" "}
+                  <Link as={RouterLink} to="/login" color={"blue.400"}>
+                    Login
+                  </Link>
+                </Text>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+    </>
   );
 }
