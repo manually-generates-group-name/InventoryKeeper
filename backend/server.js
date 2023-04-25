a7501cfc6539d519c00cba97e6ecd21d68c0e489
euclidone <96459292+euclidone@users.noreply.github.com>
2023-04-25 10:41:23 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const Router = require("./routes")
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 443;
 
 const app = express();
 
